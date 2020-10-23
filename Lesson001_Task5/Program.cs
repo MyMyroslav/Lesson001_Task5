b0693e753a380c6bdf465c451adf4e612874779f
Myroslav <havrushkomyroslav@gmail.com>
2020-10-23 13:01:59 +0300

    Dona-Lessons001_Task5

---
+++ Lesson001_Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson001_Task5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Myroslav";
+            string lastname = "Havrushko";
+            string surname = "Olehovych";
+
+            Console.WriteLine($"First Name: {name}, Last Name: {lastname}, Surname: {surname}");
+        }
+    }
+}
